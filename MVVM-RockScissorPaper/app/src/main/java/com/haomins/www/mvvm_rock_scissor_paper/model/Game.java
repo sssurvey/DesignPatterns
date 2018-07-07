cacf7ceeaef28daa2a5bf9b81cfc6819640a9b9e
haominshi <haominshi@gmail.com>
2018-07-06 22:23:51 -0500

    update

--- MVVM-RockScissorPaper/app/src/main/java/com/haomins/www/mvvm_rock_scissor_paper/model/Game.java
+++ MVVM-RockScissorPaper/app/src/main/java/com/haomins/www/mvvm_rock_scissor_paper/model/Game.java
@@ -4,6 +4,13 @@ public class Game {
 	// rock paper scissor rules
 	// 3 rounds 2 wins
 	// 2 players
+	private static final int ROUNDS = 3; // only 3 rounds
+	private static final int WIN_ROUNDS = 2; // 2 wins
+
+	public Player player1, player2;
+
+	public Player currentPlayer = player1;
+	
 
 
 }
