c81da61e3758b6c4e31ec7ebfe18668f706d2d64
haominshi <haominshi@gmail.com>
2018-07-07 22:48:20 -0500

    added

--- WorkWithDataBinding/app/src/main/java/com/haomins/www/workwithdatabinding/view/MainActivity.java
+++ WorkWithDataBinding/app/src/main/java/com/haomins/www/workwithdatabinding/view/MainActivity.java
@@ -1,14 +1,19 @@
 package com.haomins.www.workwithdatabinding.view;
 
+import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.haomins.www.workwithdatabinding.R;
 import com.haomins.www.workwithdatabinding.databinding.ActivityMainBinding;
 import com.haomins.www.workwithdatabinding.model.User;
 
 public class MainActivity extends AppCompatActivity {
+	private User user;
+	private MyClickHandlers handlers;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +22,27 @@ public class MainActivity extends AppCompatActivity {
 
 		ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-		User user = new User();
+
+		user = new User();
 		user.setName("Jadas Nakamoto");
 		user.setEmail("JNmoto@moto.rola");
-
 		binding.setUser(user);
+
+		handlers = new MyClickHandlers(this);
+		binding.setHandlers(handlers);
+
 	}
+
+	public class MyClickHandlers{
+		Context context;
+		MyClickHandlers(Context c){
+			this.context = c;
+		}
+
+		public void onButtonClicked(View view){
+			Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_LONG).show();
+		}
+	}
+
+
 }
